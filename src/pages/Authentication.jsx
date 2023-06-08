8cabad266c2c3d23687afaaf16c6d06dacac0404
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2023-06-08 23:49:25 +0300

    Update Authentication.jsx

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 
 const Authentication = () => {
     return (
-        <div className='grid justify-center sm:items-center min-h-screen'>
+        <div className='grid justify-center sm:items-center'>
             <div className='flex flex-col gap-5 pt-8 px-5 pb-4 sm:shadow rounded-sm'>
                 <div className='flex gap-5 border-b border-black text-xl'>
                     <Link to='/' className='py-2'>
