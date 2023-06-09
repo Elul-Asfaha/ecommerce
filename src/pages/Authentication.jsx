dce5a0bb0d16120711ce507f8ea57d72e8ddfb12
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2023-06-09 19:59:33 +0300

    Add files via upload

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 
 const Authentication = () => {
     return (
-        <div className='grid justify-center sm:items-center'>
+        <div className='flex flex-col pt-[100px] sm:pt-[0px] sm:min-h-screen sm:justify-center items-center'>
             <div className='flex flex-col gap-5 pt-8 px-5 pb-4 sm:shadow rounded-sm'>
                 <div className='flex gap-5 border-b border-black text-xl'>
                     <Link to='/' className='py-2'>
